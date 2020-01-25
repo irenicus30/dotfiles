074ce8f6afe5ee9632a9f6f13b918f431a38df4e
Mar Sob <irenicus30@gmail.com>
2020-01-25 17:10:55 +0100

    add startup

---
+++ startup.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp .vimrc ~/.vimrc
+cp .tmux.conf ~/.tmux.conf
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+
+
+
+
