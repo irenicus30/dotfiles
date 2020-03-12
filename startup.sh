a97b1ae5c33c3e4dee9b4b2f4cdb745b902f86fc
Marcin Sobczyk <irenicus30@gmail.com>
2020-03-12 23:21:58 +0100

    copy also .LESS_TERMCAP at startup

--- startup.sh
+++ startup.sh
@@ -1,6 +1,7 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/.vimrc
 cp .tmux.conf ~/.tmux.conf
+cp .LESS_TERMCAP.sh ~/.LESS_TERMCAP.sh
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
