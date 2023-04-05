a3d1c1c85f39260a55ef9926ce3d6c3e713b2742
Marcin Sobczyk <msobczyk@syncad.com>
2023-04-05 13:59:15 +0200

    globa gitignore

--- main.sh
+++ main.sh
@@ -2,6 +2,8 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/.vimrc
 cp .tmux.conf ~/.tmux.conf
 cp .LESS_TERMCAP.sh ~/.LESS_TERMCAP.sh
+cp .gitignore ~/.gitignore
+git config --global core.excludesfile ~/.gitignore
 
 vim -c PluginInstall
 
